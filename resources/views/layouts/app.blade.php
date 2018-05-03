13a3de64b062c8cf0eace2065f4ab8a3b20c6942
Anjulata Gupta <anjulata.gupta@maximess.com>
2018-05-03 17:45:53 +0530

    bootstrap plugin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,73 +8,55 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+
+
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-   <!--  <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <!--  <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/sb-admin.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dataTables.bootstrap4.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin.css') }}" rel="stylesheet"> -->
+     @if (Auth::guest())
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+     @else
+     <!-- Bootstrap core CSS-->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+  @endif
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+    <div id="app">      
+        <div class="container-fluid">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+            <header class="row">
+                @include('elements.topbar')
+            </header>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+            <div id="main" class="row" >
+                @if (!Auth::guest())
+                <!-- sidebar content -->
+                <div id="sidebar" class="col-md-4">
+               
+                    @include('elements.sidebar')
+
                 </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
+                @endif
+                <!-- main content -->
+                <div id="content" class="col-md-8 content-wrapper">
+                    <div class="container-fluid">
+                    @yield('content')
+                    </div>
                 </div>
-            </div>
-        </nav>
-
-        @yield('content')
+            </div>   
+        </div>        
     </div>
 
     <!-- Scripts -->
